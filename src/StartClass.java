822663f922373edd883d32d40d193dc511f8cbe4
lepeschko <buratino.by@yandex.ru>
2022-03-14 15:19:26 +0100

    Version mit dem ersten .JAR zum Testen am MAC-1

--- src/StartClass.java
+++ src/StartClass.java
@@ -4,11 +4,12 @@ import java.awt.*;
 public class StartClass {
 
 
-    public  static void main( String args[]){
+    public  static void main(String[] args){
         System.out.println("Hallo Du!");
 
         XmlReader reader = new XmlReader();
-        reader.readFromFile("Z:\\Lepeschko\\anfragenstorage\\storage1\\xmlFile11.xml");
+       // reader.readFromFile("Z:\\Lepeschko\\anfragenstorage\\storage1\\xmlFile11.xml");
+        reader.readFromFile("src\\xmlFile11.xml");
 // здесь у нас создается новый список из заявок, прочитанных из файла
         // neu Komment
 AnalyseFrame af = new AnalyseFrame();
@@ -21,7 +22,7 @@ AnalyseFrame af = new AnalyseFrame();
 
 
 
-        XmlWriter writer = new XmlWriter();
-        writer.writeXML();
+      //  XmlWriter writer = new XmlWriter();
+        XmlWriter.writeXML();
     }
 }
